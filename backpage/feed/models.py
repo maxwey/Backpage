44fa84e165f5a82e55bb282b15aa4d71eaec87cb
Maxence Weyrich <maxence.weyrich@gmail.com>
2020-08-04 00:23:47 -0700

    Changes the views to be more destructured into different files for additional reusability Adds much of the starter user interface appearance CSS and HTML logic to display posts and comments Adds comments to the models Changes how posts are stored in the database, using now ParentPosts and CommentPosts (which inherit from Posts)

--- backpage/feed/models.py
+++ backpage/feed/models.py
@@ -31,3 +31,26 @@ class Post(models.Model):
 
     def __str__(self):
         return "Post{%d, %s, %s}" % (self.id, self.author, self.text[:20])
+
+
+class CommentPost(Post):
+    """
+    This class represents a comment, with all associated media, text, author
+    data as well as interaction data, such as likes and shares.
+    Comments only differ from a Post in that they must link to another Post
+    """
+
+    parent_post = models.ForeignKey(Post, related_query_name="comment_parent_post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "CommentPost{%d, %d, %s, %s}" % (self.id, self.parent_post.id, self.author, self.text[:20])
+
+
+class ParentPost(Post):
+    """
+    This class represents a "parent post". That is, any post that was made that is
+    not a comment.
+    """
+
+    def __str__(self):
+        return "ParentPost{%d, %s, %s}" % (self.id, self.author, self.text[:20])
--- backpage/feed/views.py
+++ backpage/feed/views.py
@@ -1,19 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from .models import Post, User
+from .models import Post, CommentPost, ParentPost, User
 
 # Create your views here.
 def index(request):
     # load the data from the db
-    recents_posts = Post.objects.order_by('-create_date')
+    recents_posts = ParentPost.objects.order_by('-create_date')
+
+    # load the comments
+    # TODO: to minimize page load time, load comments as requested rather than all at once
+    data = []
+    for recent_post in recents_posts:
+        comments_qs = CommentPost.objects.filter(parent_post_id=recent_post.id)
+        # result set was empty; skip over
+        if comments_qs:
+            comments = comments_qs.order_by('create_date')
+            data.append((recent_post, comments))
+        else:
+            data.append((recent_post, ()))
 
     # load the html template used for the feed page
     template = loader.get_template('feed/index.html')
 
     # set the context for the template (define the variables used in the template)
     context = {
-        'posts': recents_posts
+        'posts': data
     }
 
     # Return the HttpResponse to the user with the rendered template
